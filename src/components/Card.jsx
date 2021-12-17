8b467d72d866cb40b62916958737c620fe48c149
zanotti86 <alexiszanotti@gmail.com>
2021-12-17 16:11:07 +0100

    Weather App

---
+++ src/components/Card.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import "./Card.css";
+import { gsap } from "gsap";
+
+export default function Card({
+  country,
+  wind,
+  temp,
+  name,
+  img,
+  pressure,
+  onClose,
+  id,
+  description,
+  humidity,
+  nextDays,
+}) {
+  //get day of the week
+  const day = new Date().getDay();
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const today = days[day];
+  const tomorrow = days[(day + 1) % 7];
+  const tomorrow1 = days[(day + 2) % 7];
+  const tomorrow2 = days[(day + 3) % 7];
+  const tomorrow3 = days[(day + 4) % 7];
+
+  function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  }
+
+  const detail = () => {
+    const p = document.querySelector(".card-right");
+    const q = document.querySelector(".container").nextSibling;
+    if (!q) {
+      gsap.to(p, { duration: 0.5, opacity: 1, x: 300, zIndex: 1 });
+      gsap.to(q, { duration: 0.5, x: 300 });
+    } else {
+      const j = q.lastChild;
+      if (j && j.style.opacity === "1") {
+        gsap.to(p, { duration: 0.5, opacity: 1, x: -300, zIndex: 1 });
+      } else {
+        gsap.to(j, { duration: 0.5, opacity: 1, x: 300, zIndex: 1 });
+      }
+    }
+  };
+
+  const closeDetail = () => {
+    const p = document.querySelector(".card-right");
+    const q = document.querySelector(".container").nextSibling;
+    if (p && !q) {
+      gsap.to(p, { duration: 0.5, opacity: 0, x: 0, zIndex: -1 });
+      gsap.to(q, { duration: 0.5, x: 0 });
+    } else {
+      const j = q.lastChild;
+      if (j && j.style.opacity === "1") {
+        gsap.to(j, { duration: 0.5, opacity: 0, x: 0, zIndex: -1 });
+      } else {
+        gsap.to(p, { duration: 0.5, opacity: 0, x: 0, zIndex: -1 });
+      }
+    }
+  };
+  console.log(nextDays);
+  return (
+    <div className='container'>
+      <div className='card-left'>
+        <button className='btn-close' onClick={onClose}>
+          X
+        </button>
+        <div className='card-header'>
+          <div className='location-card'>
+            <span>
+              <h3 onClick={detail} className='title-location'>
+                {" "}
+                <i className='fas fa-map-marker-alt'></i>
+                {country ? " " + name + ", " + country : " " + name}
+              </h3>
+            </span>
+          </div>
+          <div className='title-card'>
+            <h4>{today}</h4>
+          </div>
+        </div>
+        <div className='card-temp'>
+          <img
+            className='iconoClima'
+            src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+            width='80'
+            height='80'
+            alt='Img not found'
+          />
+          <h1>{Math.round(temp)}°C</h1>
+          <h4>{capitalize(description)}</h4>
+        </div>
+      </div>
+      <div className='card-right'>
+        <button id='btn-close1' onClick={closeDetail}>
+          X
+        </button>
+        <div className='card-rigth-header'>
+          <div className='card-humedad'>
+            <h3>HUMIDITY: {humidity} %</h3>
+            <h3>WIND: {wind} km/h</h3>
+            <h3>PRESSURE: {pressure} hPa</h3>
+          </div>
+        </div>
+
+        <div className='every-day'>
+          {nextDays.map(day => (
+            <div key={day.id} className='next-days'>
+              <div className='card-day'>
+                <img
+                  alt='Img not found'
+                  className='img-days'
+                  src={`http://openweathermap.org/img/wn/${day.day1}@2x.png`}
+                />
+                <h3 className='nexts-days-title'>{tomorrow.slice(0, 3)}</h3>
+                <h3>{Math.round(day.temp1)}°C</h3>
+              </div>
+              <div className='card-day'>
+                <img
+                  alt='Img not found'
+                  className='img-days'
+                  src={`http://openweathermap.org/img/wn/${day.day2}@2x.png`}
+                />
+                <h3 className='nexts-days-title'>{tomorrow1.slice(0, 3)}</h3>
+                <h3>{Math.round(day.temp2)}°C</h3>
+              </div>
+              <div className='card-day'>
+                <img
+                  alt='Img not found'
+                  className='img-days'
+                  src={`http://openweathermap.org/img/wn/${day.day3}@2x.png`}
+                />
+                <h3 className='nexts-days-title'>{tomorrow2.slice(0, 3)}</h3>
+                <h3>{Math.round(day.temp3)}°C</h3>
+              </div>
+              <div className='card-day'>
+                <img
+                  alt='Img not found'
+                  className='img-days'
+                  src={`http://openweathermap.org/img/wn/${day.day4}@2x.png`}
+                />
+                <h3 className='nexts-days-title'>{tomorrow3.slice(0, 3)}</h3>
+                <h3>{Math.round(day.temp4)}°C</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./App.css";
+import Nav from "../components/Nav.jsx";
+import Cards from "../components/Cards";
+import { Route, Routes } from "react-router-dom";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+function App() {
+  const [cities, setCities] = useState([]);
+
+  function onClose(id) {
+    setCities(oldCities => oldCities.filter(c => c.id !== id));
+  }
+
+  function onSearch(ciudad) {
+    //Llamado a la API del clima
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${ciudad}&appid=${apiKey}&units=metric`
+    )
+      .then(r => r.json())
+      .then(r => {
+        if (r.cod !== "404") {
+          const ciudad = {
+            name: r.city.name,
+            country: r.city.country,
+            temp: r.list[0].main.temp,
+            img: r.list[0].weather[0].icon,
+            id: r.city.id,
+            pressure: r.list[0].main.pressure,
+            wind: r.list[0].wind.speed,
+            description: r.list[0].weather[0].description,
+            humidity: r.list[0].main.humidity,
+            nextDays: [
+              {
+                id: r.city.id,
+                day1: r.list[1].weather[0].icon,
+                day2: r.list[2].weather[0].icon,
+                day3: r.list[3].weather[0].icon,
+                day4: r.list[4].weather[0].icon,
+                temp1: r.list[1].main.temp,
+                temp2: r.list[2].main.temp,
+                temp3: r.list[3].main.temp,
+                temp4: r.list[4].main.temp,
+              },
+            ],
+          };
+
+          setCities(oldCities => [...oldCities, ciudad]);
+        } else {
+          alert("Ciudad no encontrada");
+        }
+      });
+  }
+
+  return (
+    <div className='App'>
+      <Nav onSearch={onSearch} />
+      <Routes>
+        <Route path='/' element={<Cards cities={cities} onClose={onClose} />} />
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
